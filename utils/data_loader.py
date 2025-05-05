d20ced57834139f09f8c415a43fe0e3bbbd1bf21
DhirajBro1 <panditdhiraj296@gmail.com>
2025-05-05 13:53:11 +0545

    deploy tetsting

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -1,40 +1,13 @@
-# import pandas as pd
-# import streamlit as st
-# import geopandas as gpd
-
-# @st.cache_data
-# def load_weather_events():
-#     return pd.read_csv('data/processed_data/processed_extreme_weather_events.csv')
-
-# @st.cache_data
-# def load_temperature_data():
-#     return pd.read_csv('data/processed_data/dailyclimate.csv')
-
-# @st.cache_data
-# def load_glacial_data():
-#     return pd.read_csv('data/processed_data/processed_glacial_lake_data.csv')
-
-# @st.cache_data
-# def load_flood_data():
-#     return pd.read_csv('data/processed_data/flood_data.csv')
-
-# @st.cache_data
-# def load_population_data():
-#     return pd.read_csv('data/processed_data/processed_population_data.csv')
-
-# @st.cache_data
-# def load_flood_geojson():
-#     return gpd.read_file('data/processed_data/flood_data.geojson')
-
-
-
 import pandas as pd
 import streamlit as st
 import geopandas as gpd
 
 @st.cache_data
-def load_weather_events():
-    return pd.read_csv('data/processed_data/processed_extreme_weather_events.csv')
+# def load_weather_events():
+#     return pd.read_csv('data/processed_data/processed_extreme_weather_events.csv')
+def load_temperature_data():
+    file_path = Path(__file__).parent.parent / "data" / "processed_data" / "dailyclimate.csv"
+    return pd.read_csv(file_path)
 
 @st.cache_data
 def load_temperature_data():
@@ -56,23 +29,5 @@ def load_population_data():
 def load_flood_geojson():
     return gpd.read_file('data/processed_data/flood_data.geojson')
 
-def main():
-    st.title("Climate Change Data Explorer - Nepal")
-    
-    data_options = {
-        "Extreme Weather Events": load_weather_events,
-        "Temperature Data": load_temperature_data,
-        "Glacial Lake Data": load_glacial_data,
-        "Flood Data": load_flood_data,
-        "Population Data": load_population_data,
-        "Flood GeoJSON (Map)": load_flood_geojson
-    }
 
-    choice = st.sidebar.selectbox("Select Dataset", list(data_options.keys()))
-    df = data_options[choice]()
 
-    st.write(f"### Showing {choice}")
-    st.dataframe(df.head())
-
-if __name__ == "__main__":
-    main()
