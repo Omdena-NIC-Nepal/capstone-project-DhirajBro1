c24e454fffdb60190cb4c1e13fa4bbc16b6a0d15
DhirajBro1 <panditdhiraj296@gmail.com>
2025-05-10 14:13:15 +0545

    Fixed punlt error 2

--- utils/processor.py
+++ utils/processor.py
@@ -1,8 +1,6 @@
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 import re
-import nltk
-nltk.download('punkt')
 from sumy.parsers.plaintext import PlaintextParser
 from sumy.nlp.tokenizers import Tokenizer
 from sumy.summarizers.lex_rank import LexRankSummarizer
@@ -57,7 +55,8 @@ def clean_topics(topic_string):
                 cleaned.append((topic_name, keywords_list))
     return cleaned
 def summarize_text(text, num_sentences=3):
-    
+    import nltk
+    nltk.download('punkt')
     parser = PlaintextParser.from_string(text, Tokenizer("english"))
     summarizer = LexRankSummarizer()
     summary = summarizer(parser.document, num_sentences)
